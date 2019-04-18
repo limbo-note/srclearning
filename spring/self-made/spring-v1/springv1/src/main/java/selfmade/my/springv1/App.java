24fe624a0cf91f030e1c76a072ff120bf60be810
limbo <limbo_china@163.com>
2019-04-18 22:13:57 +0800

    4-18

---
+++ spring/self-made/spring-v1/springv1/src/main/java/selfmade/my/springv1/App.java
@@ -0,0 +1,13 @@
+package selfmade.my.springv1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
